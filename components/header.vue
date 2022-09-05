eb653a4c23ad78c5db26f62e5daff320f3514612
Nicolas Servary <nicolas.servary@epsi.fr>
2022-09-05 16:57:21 +0200

    jour 1

---
+++ components/header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="gros-block">
+    <div class="d-flex justify-content-between">
+      <div class="mx-4 mt-2"><img src="" alt="logo"></div>
+      <div class="mx-4 mt-2"><p class="text-white"><a href="" class="style">Contact</a></p></div>
+    </div>
+    <div class="text-center">
+      <input id="searchbar" onkeyup="" type="text" name="search" placeholder="Search animals.." class="rounded-pill border-0 search-consol">
+    </div>
+    <div class="d-flex justify-content-end">
+      <div class="mt-4 mr-4">
+        <img src="" alt="logo-facebook">
+        <img src="" alt="logo-insta">
+        <img src="" alt="logo-twitter">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  .gros-block {
+    background-color: #1F7093;
+    height: 20vh;
+  }
+  .search-consol {
+    width: 45vw;
+    height: 5vh;
+  }
+  a {
+    color: white;
+  }
+
+
+</style>
